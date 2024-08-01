94eb8d42c8ab3c104a2a6d9d136d303a1c13f9ef
irodotos <irodotos@vaslabs.io>
2024-08-01 12:28:44 +0300

    refactor

--- build.sbt
+++ build.sbt
@@ -6,21 +6,7 @@ lazy val root = (project in file("."))
   .settings(
     name := "stream-zip"
   )
-  .dependsOn(zipPartitioner, dynamoZipStore)
-
-libraryDependencies += "co.fs2" %% "fs2-core" % "3.10.2"
-libraryDependencies += "co.fs2" %% "fs2-io" % "3.10.2"
-libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.4"
-libraryDependencies += "org.specs2" %% "specs2-core" % "4.20.7" % "test"
-// https://mvnrepository.com/artifact/net.lingala.zip4j/zip4j
-libraryDependencies += "net.lingala.zip4j" % "zip4j" % "2.11.5"
-
-
-libraryDependencies ++= Seq(
-  "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.12.732",
-  "commons-io" % "commons-io" % "2.16.1"
-)
-
+  .aggregate(zipPartitioner, dynamoZipStore)
 
 // from a list of files to stream fileArchive
 lazy val zipPartitioner = (project in file("zip-partitioner"))
@@ -44,5 +30,9 @@ lazy val dynamoZipStore = (project in file("dynamo-zip-store"))
   )
   .dependsOn(zipPartitioner)
   .settings(
-    libraryDependencies += "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.12.732"
-  )
+    // https://mvnrepository.com/artifact/software.amazon.awssdk/aws-sdk-java
+    libraryDependencies += "software.amazon.awssdk" % "aws-sdk-java" % "2.26.25" % "provided",
+    // https://mvnrepository.com/artifact/software.amazon.awssdk/dynamodb
+    libraryDependencies += "software.amazon.awssdk" % "dynamodb" % "2.26.25"
+
+)
--- zip-partitioner/src/test/scala/zip_partitioner_test/ZipPartitionerSpec/ZipPartitionSpec.scala
+++ zip-partitioner/src/test/scala/zip_partitioner_test/ZipPartitionerSpec/ZipPartitionSpec.scala
@@ -1,22 +1,137 @@
 package zip_partitioner_test.ZipPartitionerSpec
+import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import fs2.Stream
+import fs2.io.file.{Files, Path}
 import zip_partitioner.ZipPartitioner.{compress, createStreamArchive, encodeBase64}
 import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
 import zip_partitioner.FileArchive
+import zip_partitioner.ZipBuilder.createZipFile
+
+import java.io.{File, FileInputStream, FileOutputStream, IOException}
+import java.security.MessageDigest
+import java.util.zip.{ZipEntry, ZipInputStream}
 
 object ZipPartitionerSpec extends Specification {
 
   "ZipPartitioner" should {
     "createStreamArchive" in {
-      val filePaths = List("src/files/file1.txt", "src/files/file2.txt")
+      val filePaths = List("zip-partitioner/src/files/file1.txt", "zip-partitioner/src/files/file2.txt")
       val result = createStreamArchive(filePaths)
       result.compile.toList.unsafeRunSync().size must_== 2
-
-//      result.compile.toList.unsafeRunSync() must_==(
-      //        FileArchive("src/files/file1.txt", encodeBase64(compress("file1".getBytes))),
-      //        FileArchive("src/files/file2.txt", encodeBase64(compress("file2".getBytes)))
-      //      )
     }
+  }
+
+  "ZipBuilder" should {
+    "createZipFile" in new LocalScope {
+      val stream = createStreamArchive(paths)
+      createZipFile(saveTo, stream).unsafeRunSync()
+      val exsistingFiles = hashFilesInDirectory("zip-partitioner/src/files/testFiles").unsafeRunSync()
+
+      unzip(saveTo, "zip-partitioner/src/files/testFilesAfterZip")
+      val unzipFiles = hashFilesInDirectory("zip-partitioner/src/files/testFilesAfterZip").unsafeRunSync()
+
+      mapsAreConsistent(exsistingFiles, unzipFiles) must_== true
+
+      val randomMap: Map[String, String] = Map("random" -> "random")
+      mapsAreConsistent(exsistingFiles, randomMap) must_== false
+    }
+  }
+
+  trait LocalScope extends Scope {
+    val paths = List("zip-partitioner/src/files/testFiles/file1.txt", "zip-partitioner/src/files/testFiles/file2.txt")
+
+    val saveTo = "zip-partitioner/src/files/testFilesAfterZip/zipFile.zip"
+
+    def computeHash(file: Path, algorithm: String = "SHA-256"): IO[String] = {
+      Files[IO].readAll(file)
+        .through(fs2.hash.digest[IO](MessageDigest.getInstance(algorithm)))
+        .compile
+        .to(Array)
+        .map(bytes => bytes.map("%02x".format(_)).mkString)
+    }
+
+    def listFiles(directory: Path): Stream[IO, Path] = {
+      Files[IO].walk(directory).filter(Files[IO].isRegularFile(_).unsafeRunSync())
+    }
+
+    def hashFilesInDirectory(directory: String): IO[Map[String, String]] = {
+      val dirPath = Path(directory)
+      listFiles(dirPath).evalMap { file =>
+        computeHash(file).flatMap { hash =>
+          IO((file.toString.split('/').last, hash))
+        }
+      }.compile.to(Map)
+    }
+
+
+    def unzip(zipFilePath: String, destDir: String): Unit = {
+      val buffer = new Array[Byte](1024)
+
+      // Ensure the destination directory exists
+      val dir = new File(destDir)
+      if (!dir.exists()) dir.mkdirs()
+
+      val zis = new ZipInputStream(new FileInputStream(zipFilePath))
+
+      try {
+        var zipEntry: ZipEntry = zis.getNextEntry
+
+        while (zipEntry != null) {
+          val newFile: File = newFileF(new File(destDir), zipEntry)
+
+          if (zipEntry.isDirectory) {
+            if (!newFile.isDirectory && !newFile.mkdirs()) {
+              throw new IOException("Failed to create directory " + newFile)
+            }
+          } else {
+            // Fix for Windows-created archives
+            val parent = newFile.getParentFile
+            if (!parent.isDirectory && !parent.mkdirs()) {
+              throw new IOException("Failed to create directory " + parent)
+            }
+
+            // Write file content
+            val fos = new FileOutputStream(newFile)
+            try {
+              var len = zis.read(buffer)
+              while (len > 0) {
+                fos.write(buffer, 0, len)
+                len = zis.read(buffer)
+              }
+            } finally {
+              fos.close()
+            }
+          }
+
+          zipEntry = zis.getNextEntry
+        }
+      } finally {
+        zis.closeEntry()
+        zis.close()
+      }
+    }
+
+    def newFileF(destinationDir: File, zipEntry: ZipEntry): File = {
+      val destFile = new File(destinationDir, zipEntry.getName)
+
+      val destDirPath = destinationDir.getCanonicalPath
+      val destFilePath = destFile.getCanonicalPath
+
+      if (!destFilePath.startsWith(destDirPath + File.separator)) {
+        throw new IOException("Entry is outside of the target dir: " + zipEntry.getName)
+      }
+
+      destFile
+    }
+
+    def mapsAreConsistent(smallMap: Map[String, String], largeMap: Map[String, String]): Boolean = {
+      smallMap.forall { case (key, value) =>
+        largeMap.get(key).contains(value)
+      }
+    }
+
   }
 }
 
